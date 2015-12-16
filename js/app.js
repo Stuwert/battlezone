79179f1f44603e4836b8cfe780cbd6a4a56acff5
Stuart Urback <stuart@urback.net>
2015-12-16 10:39:45 -0700

    Active/inactive cmoplete

--- js/app.js
+++ js/app.js
@@ -10,71 +10,42 @@ var gameObj = {
 
 $(document).ready(function(){
 
-  var player1draginto = dragula([document.querySelector(".player1bench"), document.querySelector(".act1")],{
-    copy: true
-  });
-  var player1dragoutof = dragula([document.querySelector(".act1")], {
-    removeOnSpill: true
-  });
-  player1draginto.on("drop", function(el, target, source){
-    console.log(target);
-    if(source.className.includes("bench")){
-      if(target.className.includes("act1")){
-        gameObj["player1"][+(el.className[0])].status = "active";
-        // alert("Bing");
-      }
-    }
-  })
 
-  player1dragoutof.on("remove", function(el){
-    gameObj["player1"][+(el.className[0])].status = "benched";
-    console.log(gameObj["player1"]);
-  })
 
-  dragula([document.querySelector(".player2bench"), document.querySelector(".act2")],{
-    copy: true
-  });
-  dragula([document.querySelector(".act2")], {
-    removeOnSpill: true
-  });
-
-  $(document).on("keydown", "#" + turnstatus, addToParty);
+  $(document).on("keydown", "input", addToParty);
 
 
 })
 
 function addToParty(key){
-  var classist = $(this).attr('id');
   var input = $('input').val();
   if (key.keyCode === 13){
-    var inputz = nextAvailable(gameObj[classist])
+    var inputz = nextAvailable(gameObj[turnstatus])
+    if (inputz !== null){
+      gameObj[turnstatus][inputz] = new newGameObj(array[input].name, array[input].img, array[input].status);
+      renderer(turnstatus, inputz);
+    }
+    if (nextAvailable(gameObj[turnstatus]) === null){
+      $('#'+turnstatus).prepend('<p>Party Complete</p>');
+      if (turnstatus === "player2"){
+        $('p').removeClass('hidden');
+        $('input').addClass('hidden');
+        activateDragging();
+      }
+    }
     if(turnstatus === "player1"){
       turnstatus = "player2";
-      $("#player1").parent().removeClass('turn')
-      $("#player2").parent().addClass('turn')
-      $(document).off("keydown", "#player1");
-      $(document).on("keydown", "#player2", addToParty);
+      $("#player1").removeClass('turn')
+      $("#player2").addClass('turn')
     }else{
       turnstatus = "player1";
-      $("#player2").parent().removeClass('turn')
-      $("#player1").parent().addClass('turn')
-      $(document).off("keydown", "#player2");
-      $(document).on("keydown", "#player1", addToParty);
-    }
-    if (inputz !== null){
-      gameObj[classist][inputz] = array[input];
-      renderer(classist, inputz);
-    }else{
-      // alert("BingBong")
-    }
-    if (nextAvailable(gameObj[classist]) === null){
-      $('.'+classist+'bench').find('input').after('<p>Party Complete</p>');
+      $("#player2").removeClass('turn')
+      $("#player1").addClass('turn')
     }
   }
 }
 
 function nextAvailable(playerobj){
-  console.log(playerobj)
   for (pieces in playerobj){
     if (playerobj[pieces] === null){
       return pieces;
@@ -85,9 +56,49 @@ function nextAvailable(playerobj){
 
 function renderer(inputName, divClass){
   var obj = gameObj[inputName][divClass]
-  var printTo = $("#" + inputName).siblings('.' + divClass);
-  // console.log(printTo);
+  var printTo = $('<div class="'+ divClass + '"></div>').appendTo("#" + inputName);
   $(printTo).append('<h4>' + obj.name + '</h4>');
-  $(printTo).append('<img class ="'+ divClass + '"width="80" height="80" src="'+obj.img+'"/>');
+  $(printTo).append('<img width="80" height="80" src="'+obj.img+'"/>');
 
+}
+
+function activateDragging(){
+  var player1draginto = dragula([document.querySelector(".player1bench"), document.querySelector(".act1")],{
+    copy: true
+  });
+  var player1dragoutof = dragula([document.querySelector(".act1")], {
+    removeOnSpill: true
+  });
+
+  player1draginto.on("drop", function(el){
+    gameObj["player1"][+(el.className[0])].status = "active";
+  })
+
+  player1dragoutof.on("remove", function(el){
+    gameObj["player1"][+(el.className[0])].status = "bench";
+    console.log(gameObj["player1"])
+  })
+
+  var player2draginto = dragula([document.querySelector(".player2bench"), document.querySelector(".act2")],{
+    copy: true
+  });
+  var player2dragoutof = dragula([document.querySelector(".act2")], {
+    removeOnSpill: true
+  });
+
+  player2draginto.on("drop", function(el){
+    gameObj["player2"][+(el.className[0])].status = "active";
+  })
+
+  player2dragoutof.on("remove", function(el){
+    gameObj["player2"][+(el.className[0])].status = "bench";
+    console.log(gameObj["player1"])
+  })
+
+}
+
+function newGameObj(name, image, status){
+  this.name = name;
+  this.status = status;
+  this.img = image;
 }
